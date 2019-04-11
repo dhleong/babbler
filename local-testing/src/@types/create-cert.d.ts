39d17bead96b08973e67d6e3d9e21ef7ad2fe930
Daniel Leong <me@dhleong.net>
2019-04-11 18:55:59 -0400

    Remove unused code, dependencies

--- local-testing/src/@types/create-cert.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-
-declare module "create-cert" {
-    export default function createCert(): Promise<{
-        caCert: string,
-        cert: string,
-        key: string,
-    }>;
-}
